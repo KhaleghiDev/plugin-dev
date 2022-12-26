f086462746a70ba2d8ebe2019bbf876b2f5b9619
Hamed Moodi <hamedmoodi2011@gmail.com>
2022-12-26 16:20:27 +0330

    Start Wordpress plugin developemnt tutorial

---
+++ wp-content/plugins/daneshjooyar-seo/daneshjooyar-seo.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Plugin Name: Daneshjooyar Seo
+ * Plugin URI: https://daneshjooyar.com/plugins/daneshjooyar-seo
+ * Description: اولین <strong>افزونه</strong> من در دوره آموزشی افزونه نویسی <a href="https://wordpress.org">وردپرس</a>
+ * Version: 1.0.0
+ * Requires at least: 5.1
+ * Requires PHP: 7.4
+ * Author: Hamed Moodi
+ * Author URI: https://daneshjooyar.com/teacher/h_m_mood
+ * Licence: GPLv2 or later
+ */
+
+/*
+This plugin is free seo
+Copyright (C) 2023  Hamed Moodi
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
